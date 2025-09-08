04fda2ab9cc51906976dce8a8c32e5aeb488d96a
Aravind Potharaju <aravindpotharaju123@gmail.com>
2025-09-08 18:15:18 +0530

    added new file

---
+++ src/Recursion_Array_Problems/IsArraySorted_1.java
@@ -0,0 +1,22 @@
+package Recursion_Array_Problems;
+
+public class IsArraySorted_1 {
+    public static void main(String[] args) {
+        int arr[]={1,3,3,5,7,9};
+        System.out.println(isArraySorted(arr,0));
+    }
+    static boolean isArraySorted(int []arr,int index){
+        if(index==arr.length-1){
+            return true;
+        }
+
+        // break the problem : first check for the first pair and ask the same function for remaining
+        if(arr[index]<=arr[index+1]){
+            return isArraySorted(arr,index+1);
+        }
+        else{
+            // if anywhere the elements are not in order return false
+            return false;
+        }
+    }
+}
